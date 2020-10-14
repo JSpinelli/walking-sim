7ce8c42f1bbd31efba5f2b602f4f54647ea14d9c
Julian <julianspi95@gmail.com>
2020-10-14 16:12:13 -0300

    ORganized Main Scene, set up main script for all interactable items to subscribe

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,30 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-abstract public class Interactable: MonoBehaviour
-{
+abstract public class Interactable : MonoBehaviour {
     public BoxCollider promptCollider;
     public GameObject keyPrompt;
 
     private bool isColliding = false;
     protected bool wasActivated = false;
 
+    private string location;
 
-    private void OnTriggerEnter(Collider other)
-    {
+    public NarratorPace narrator;
+
+    private void Awake () {
+        Transform t = this.transform;
+        while (t.parent != null) {
+            //Debug.Log(t.parent);
+            if (t.parent.tag == "Location") {
+                
+                location = t.parent.name;
+                narrator = GameObject.Find ("Narrator").GetComponent<NarratorPace> ();
+                narrator.Register ( location, "activable", ""+this.GetInstanceID());
+            }
+            t = t.parent.transform;
+        }
+        return; // Could not find a parent with given tag.
+    }
+
+    private void OnTriggerEnter (Collider other) {
         if (isColliding) return;
         isColliding = true;
-        keyPrompt.SetActive(true);
+        keyPrompt.SetActive (true);
     }
 
-    private void OnTriggerExit(Collider other)
-    {
+    private void OnTriggerExit (Collider other) {
         if (!isColliding) return;
         isColliding = false;
-        keyPrompt.SetActive(false);
+        keyPrompt.SetActive (false);
     }
-    
-    public  abstract void OnInteract();
+
+    public abstract void OnInteract ();
 }
---
+++ Assets/Scripts/NarratorPace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarratorPace : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void notifyComplete(string location, string type){
+
+    }
+
+    public void Register(string location, string type, string id){
+        Debug.Log("Register by location: "+location+ "Type: "+type+" Id:"+id);
+    }
+}
